6d441d9970993114882f3d5e8c453476f78d4862
leandrodalbo <leandro.edbo@gmail.com>
2025-04-06 16:28:04 +0100

    chart key generation test

--- src/test/kotlin/com/chaincharts/service/ChartInfoServiceTest.kt
+++ src/test/kotlin/com/chaincharts/service/ChartInfoServiceTest.kt
@@ -17,4 +17,12 @@ class ChartInfoServiceTest {
         assertThat(result.chartTitle).contains(TimeValue.I365.toString())
         assertThat(result.assetKey).isNotEmpty()
     }
+
+    @Test
+    fun shouldGenerateTheChartKey() {
+        val result = chartInfoService.generateChartKey("/chart/somevalue?p=days200&format=json")
+
+        assertThat(result).isNotEmpty()
+        assertThat(result).isEqualTo("ae8bc4aa63c04aedec477e7fc33908f3cd8ae73d6bffd9827b91f8fcc2decf04")
+    }
 }
